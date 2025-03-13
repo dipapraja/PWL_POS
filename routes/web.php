bfb137faab5cc8c427a0d6fc173618e622b40f29
praja09 <dipaprajadpp@gmail.com>
2025-03-13 20:24:37 +0700

    Jobsheet 3 Praktikum 4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
