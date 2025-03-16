cceb128766d13f365ad5545f30a19e7ecf301176
praja09 <dipaprajadpp@gmail.com>
2025-03-16 20:32:01 +0700

    Jobsheet 4 Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,17 +17,19 @@ class UserController extends Controller
         //     'level_id' => 6
         // ];
         // UserModel::insert($data); // tambahkan data ke tabel m_user
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
         // UserModel::where('username', 'customer-1')->update($data);
-        UserModel::create($data);
+        // UserModel::create($data);
 
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
