b05122a2932d64fd998227a10bbaacc058dce9a9
praja09 <dipaprajadpp@gmail.com>
2025-04-04 20:26:08 +0700

    Jobsheet 5 Tugas Tabel m_level

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -9,10 +9,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class LevelModel extends Model
 {
     use HasFactory;
-
-    protected $table = "m_level";
-    protected $primaryKey = "level_id";
-
+    protected $table = 'm_level';
+    protected $primaryKey = 'level_id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['level_kode', 'level_nama'];
     public function users(): HasMany
     {
         return $this->hasMany(UserModel::class);
